7b0f97dbfbf234e04e3db0a57ba537e110bd454d
Harsh Bansal <harshbansal8705@gmail.com>
2024-03-01 22:28:44 +0530

    Added nodemailer

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -3,10 +3,9 @@ const { User } = require("../../models/User");
 exports.register = async (req, res) => {
     const user = new User(req.body);
     try {
-        await user.validate();
+        await user.save();
     } catch (e) {
         return res.status(400).json({ success: false, message: e });
     }
-    await user.save();
     console.log("User registered successfully" , user);
 }
